43b1ec62d08b375a1743a3bf42e55f9da5d025f2
Shaun Ellis <shaun@sdellis.com>
2017-10-31 16:41:57 -0400

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: red;
+}
+p {
+  font-size: 2em;
+  text-align: center;
+  color: red;
+}
+</style>
